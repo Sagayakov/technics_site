86ce680f39c6defe03a18660a56633056e953e1f
Sagayakov <sadayfrom@gmail.com>
2023-07-19 20:01:52 +0500

    Добавил посчетчик лайков в сериализатор

--- website/technics/serializers.py
+++ website/technics/serializers.py
@@ -1,12 +1,18 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from technics.models import Technics, Mark, Category, UserTechRelation
 
 
 class TechSerializer(ModelSerializer):
+    likes_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Technics
         fields = '__all__'
+
+    def get_likes_count(self, instance):
+        return UserTechRelation.objects.filter(technics=instance, like=True).count()
 
 
 class MarkSerializer(ModelSerializer):

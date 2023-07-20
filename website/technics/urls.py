8072b08805743098010fd5c33007767886ac761e
Sagayakov <sadayfrom@gmail.com>
2023-07-20 16:25:50 +0500

    Добавлен url для Like

--- website/technics/urls.py
+++ website/technics/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('search/', views.Search.as_view(), name='search'),
     path('<slug:slug>/', views.TechnicDetailView.as_view(), name='technic_detail_url'),
     path('review/<int:pk>/', views.AddCommentsView.as_view(), name='add_comments_url'),
-
+    path('like/<int:pk>/', views.post_rating, name='add_ratings_url'),
 ]

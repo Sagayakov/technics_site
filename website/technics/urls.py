f29aea76a020435b8b9a5287a9925a9161709cbf
Sagayakov <sadayfrom@gmail.com>
2023-07-24 19:48:55 +0500

    Добавлен URL для работы с рейтингом

--- website/technics/urls.py
+++ website/technics/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('', views.TechnicsView.as_view()),
     path('filter/', views.FilterTechView.as_view(), name='filter_url'),
     path('search/', views.Search.as_view(), name='search'),
-    path('<slug:slug>/', views.TechnicDetailView.as_view(), name='technic_detail_url'),
     path('review/<int:pk>/', views.AddCommentsView.as_view(), name='add_comments_url'),
-    path('like/<int:pk>/', views.post_rating, name='add_ratings_url'),
+    path('like/<int:pk>/', views.post_like, name='add_like_url'),
+    path('rating/<int:pk>/', views.post_rating, name='add_rating_url'),
+    path('<slug:slug>/', views.TechnicDetailView.as_view(), name='technic_detail_url'),
+
 ]

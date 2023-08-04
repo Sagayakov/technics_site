e4f6f563365e19c60c5915ff4e5fca513b99ae07
Sagayakov <sadayfrom@gmail.com>
2023-08-04 20:00:30 +0500

    Добавил аннотированные поля для снижения кол-ва sql запросов Так же добавил вывод создателя объекта и юзеров взаимодействующих с объектом

--- website/technics/serializers.py
+++ website/technics/serializers.py
@@ -1,18 +1,33 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from technics.models import Technics, Mark, Category, UserTechRelation
 
 
+class TechClientsSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name')
+
+
 class TechSerializer(ModelSerializer):
-    likes_count = serializers.SerializerMethodField()
+    # likes_count = serializers.SerializerMethodField()
+    likes_annotated = serializers.IntegerField(read_only=True)
+    rating_annotated = serializers.DecimalField(max_digits=3, decimal_places=2, read_only=True)
+    owner_name = serializers.CharField(source='owner.username', default='', read_only=True)
+    clients = TechClientsSerializer(many=True, read_only=True)
 
     class Meta:
         model = Technics
-        fields = '__all__'
+        fields = (
+            'id', 'mark', 'model', 'price', 'small_description', 'description',
+            'year', 'category', 'slug', 'youtube', 'is_public', 'likes_annotated',
+            'rating_annotated', 'owner_name', 'clients'
+        )
 
-    def get_likes_count(self, instance):
-        return UserTechRelation.objects.filter(technics=instance, like=True).count()
+    # def get_likes_count(self, instance):
+    #     return UserTechRelation.objects.filter(technics=instance, like=True).count()
 
 
 class MarkSerializer(ModelSerializer):
